f46d8299fbaef2ac4bdb7031552d247674c004c6
Zsolt Zsuffa <zsolt.zsuffa@gmail.com>
2021-02-25 08:27:47 +0100

    first commit

---
+++ user-management/user.cmd.api/src/main/java/com/springbank/user/cmd/api/UserCommandApplication.java
@@ -0,0 +1,21 @@
+package com.springbank.user.cmd.api;
+
+import com.springbank.user.cmd.api.configuration.ApplicationSecurity;
+import com.springbank.user.core.configuration.AxonConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableResourceServer
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@SpringBootApplication
+@Import({ AxonConfig.class, ApplicationSecurity.class })
+public class UserCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCommandApplication.class, args);
+	}
+
+}
